07296fd21d2ce76e2781a0d2a992de1b1e8b17aa
testinfer <47186542+testinfer@users.noreply.github.com>
2019-02-05 20:22:02 +0100

    Update main.c

--- main.c
+++ main.c
@@ -1,41 +1,14 @@
-int main() {
-	
-	return 0;
+int main () {
+ 	char x[10];
+	char * y = &(*x);
+	int i = 0 ;
+	while (i < 10) {
+		i = i+1;
+		if (i == 5) {
+			*y = 0;
+		} else {
+			*y = 1;
+		}
+		y = y+1;
+	}
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
